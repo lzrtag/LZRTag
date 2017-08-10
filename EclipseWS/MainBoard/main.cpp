2f65956348e6c9813da3834bcebbfe487568dbe2
Xasin <logger89@web.de>
2017-08-10 19:17:53 +0200

    o.o' | Just ... Enjoy the code, K? >.>

--- EclipseWS/MainBoard/main.cpp
+++ EclipseWS/MainBoard/main.cpp
@@ -22,27 +22,46 @@
 
 #include "Localcode/ESPComs/ESPUART.h"
 
+volatile bool hasBeenShot = false;
+
+uint8_t playerID = 0;
+
+ESPComs::Endpoint PlayerIDEP(100, &playerID, 1, 0);
 
 ESPComs::Endpoint ColorEP(101, &Board::Vest::team, 1, 0);
 ESPComs::Endpoint VestBrightnessEP(200, &Board::Vest::mode, 1, 0);
 
-struct {
+struct BuzzCommand {
 	uint8_t length;
 	uint8_t startFreq;
 	uint8_t endFreq;
-} buzzCommand;
+};
 void playPing() {
+	BuzzCommand buzzCommand = *(BuzzCommand*)&ESPComs::Endpoint::pubBuffer;
+
 	Board::Buzzer::sweep(buzzCommand.startFreq*60, buzzCommand.endFreq*60, buzzCommand.length*10);
 }
-ESPComs::Endpoint PingEndpoint(11, &buzzCommand, 3, playPing);
+ESPComs::Endpoint PingEndpoint(11, &ESPComs::Endpoint::pubBuffer, 3, playPing);
 
-uint8_t vibrDuration;
 void playVibration() {
-	Board::Vibrator::vibrate(vibrDuration*10);
+	Board::Vibrator::vibrate(ESPComs::Endpoint::pubBuffer[0]*10);
 }
-ESPComs::Endpoint VibrationEP(10, &vibrDuration, 1, playVibration);
+ESPComs::Endpoint VibrationEP(10, &ESPComs::Endpoint::pubBuffer, 1, playVibration);
+
+void handleShots() {
+	if(ESPComs::Endpoint::pubBuffer[0] == 99) {
+		Board::Vibrator::vibrate(200);
+		Board::Nozzle::flash(0b10 << Board::Vest::team);
+		Board::Buzzer::sweep(3000, 1000, 150);
+		IR::TX::startTX({playerID, 0});
+	}
+}
+ESPComs::Endpoint ShootCommandEP(0, &ESPComs::Endpoint::pubBuffer, 1, handleShots);
 
 void IRRXCB(IR::ShotPacket data) {
+	if(data.playerID == playerID)
+		return;
+	hasBeenShot = true;
 }
 
 int main() {
@@ -55,7 +74,15 @@ int main() {
 
 	IR::RX::setCallback(IRRXCB);
 
+	Board::Nozzle::flash(1);
+
 	while(true) {
+		while(!hasBeenShot) {}
+		Board::Vibrator::vibrate(1000);
+		Board::Vest::mode = 5;
+		_delay_ms(1000);
+		Board::Vest::mode = 0;
+		hasBeenShot = false;
 	}
 	return 0;
 }
