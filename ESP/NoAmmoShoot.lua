14bf2f9829ffb706feb77e787a6b9cf686c5a9cc
Xasin <logger89@web.de>
2017-08-11 14:25:26 +0200

    Added a hook to support "dead time" when hit

--- ESP/NoAmmoShoot.lua
+++ ESP/NoAmmoShoot.lua
@@ -5,8 +5,23 @@ shotsRunning	= false;
 
 shotTimer = tmr.create();
 
+lastHitTimestamp = 0;
+
+function canShoot()
+	tSinceHit = tmr.now() - lastHitTimestamp;
+	if(tSinceHit < 0) then
+		tSinceHit = tSinceHit + 2147483647;
+	end
+
+	if(tSinceHit < 3000000) then
+		return false;
+	end
+
+	return true;
+end
+
 function shootIfValid()
-	if(not triggerPressed) then
+	if(not triggerPressed or not canShoot()) then
 		shotTimer:stop();
 		shotsRunning = false;
 	else
@@ -17,7 +32,7 @@ end
 registerUARTCommand(0, 1,
 	function(data)
 		triggerPressed = (data:byte() ~= 0);
-		if(triggerPressed and not shotsRunning) then
+		if(triggerPressed and not shotsRunning and canShoot()) then
 			shootIfValid();
 			shotTimer:start();
 			shotsRunning = true;
@@ -26,3 +41,10 @@ registerUARTCommand(0, 1,
 );
 
 shotTimer:register(250, tmr.ALARM_AUTO, shootIfValid);
+registerUARTCommand(1, 2,
+	function(data)
+		lastHitTimestamp = tmr.now();
+
+		print("Hit by:" .. data:byte(1) .. "," .. data:byte(2) .. " DLen: " .. data:len());
+	end
+);
