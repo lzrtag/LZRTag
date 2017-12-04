473070a0dc9d56a60e593eff514d636ca5485595
Xasin <logger89@web.de>
2017-12-04 10:38:24 +0100

    Added extra blink modes :3

--- EclipseWS/MainBoard/Localcode/Board/VestBlink.cpp
+++ EclipseWS/MainBoard/Localcode/Board/VestBlink.cpp
@@ -14,6 +14,10 @@ namespace Vest {
 uint8_t PWMPresc = SOFTPWM_LEN-1;
 uint8_t LEDPWM[3];
 
+uint8_t team = 0;
+uint8_t mode = 1;
+uint8_t phase = 0;
+
 uint8_t slowPresc = 100;
 void update() {
 	if(PWMPresc == 0)
@@ -41,10 +45,11 @@ void lightsOn() {
 	for(uint8_t i=0; i<3; i++)
 		LEDPWM[i] = SOFTPWM_LEN;
 }
-
-uint8_t team = 0;
-uint8_t mode = 1;
-uint8_t phase = 0;
+void teamColorOn(uint8_t brightness) {
+	for(uint8_t i=0; i<3; i++)
+		if(((1<<i) & team) != 0)
+			LEDPWM[i] = brightness;
+}
 
 BlinkOverrides overrides = {0, 0};
 
@@ -62,28 +67,28 @@ void slowUpdate() {
 	}
 
 	switch(lMode) {
-	default:
 	case 0:
 		if(phase == 0)
-			LEDPWM[team] = 1;
+			teamColorOn(1);
 	break;
 
 	case 1:
 		if((phase%6) == 0)
-			LEDPWM[team] = 1;
+			teamColorOn(1);
 	break;
 
+	default:
 	case 3:
-		LEDPWM[team] = 2;
+		teamColorOn(2);
 	case 2:
 		if((phase%6) == 0)
-			LEDPWM[team] = 10;
+			teamColorOn(SOFTPWM_LEN);
 	break;
 
 	case 4:
-		LEDPWM[team] = 4;
+		teamColorOn(4);
 		if((phase%6) == 0)
-			LEDPWM[team] = 10;
+			teamColorOn(10);
 	break;
 
 	case 5:
@@ -91,7 +96,20 @@ void slowUpdate() {
 			lightsOn();
 		}
 		else
-			LEDPWM[team] = 10;
+			teamColorOn(4);
+	break;
+
+	case 6:
+		if(phase < 6)
+			teamColorOn(phase/2);
+		else
+			teamColorOn(6 - phase/2);
+	break;
+	case 7:
+		if(phase < 6)
+			teamColorOn(phase + 1);
+		else
+			teamColorOn(12 - phase);
 	break;
 
 	case 10:
