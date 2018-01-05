f1d57a12f0948df348c4d74931eb48c8bd1cae68
Xasin <logger89@web.de>
2018-01-05 15:49:50 +0100

    New player behavior :D

--- Ruby/Sinatra/PlayerListTest.rb
+++ Ruby/Sinatra/PlayerListTest.rb
@@ -3,16 +3,20 @@
 require 'json'
 require_relative '../GameInterface/LTagGame.rb'
 
-$mqtt = MQTT::SubHandler.new('iot.eclipse.org');
+$mqtt = MQTT::SubHandler.new('localhost');
 $game = Lasertag::Game.new($mqtt);
-
-require_relative 'BaseClasses/JSONBase'
 
 $game.on_connect do |pName, player|
 	player.ammo = 1000;
 
+	player.team = rand(1..6);
+	player.brightness = 7;
+
 	player.hitConfig = {
-		dieOnHit: true,
+		hitFlashBrightness: 10,
+		hitFlashDuration: 180,
+		hitVibration: 100,
+
 		deathDuration: 5000,
 	}
 
@@ -20,26 +24,40 @@ $game.on_connect do |pName, player|
 		shotLocked: false,
 	}
 
-	player.team = rand(1..7);
+	player.data[:hitpoints] = 10;
 end
 
-$mqtt.subscribe_to "Lasertag/Game/Events" do |tList, data|
+require_relative 'BaseClasses/JSONBase'
+
+$mqtt.subscribe_to "Lasertag/Game/Events" do |topic, data|
 	begin
 		data = JSON.parse(data);
-
 		if(data["type"] == "hit") then
-			target = $game[data["target"]];
-			unless(target.dead?) then
-				shooter = $game[data["shooterID"].to_i];
-				shooter.noise();
-
-				target.team 		= rand(1..7);
-				target.brightness = rand(0..7);
+			player = $game[data["target"]];
+			if(player.data[:hitpoints] -= 1) <= 0 then
+				player.data[:hitpoints] = 0;
+				player.dead = true;
+			else
+				player.hit
 			end
+			$game[data["shooterID"]].noise(duration: 0.02, startF: 2000);
 		end
 	rescue
 	end
 end
 
+Thread.new do loop do
+	sleep 0.1;
+	$game.each_connected do |pName, player|
+		if(player.data[:hitpoints] < 10 and not player.dead?) then
+			player.data[:hitpoints] += 0.02;
+		end
+
+		player.heartbeat = ((player.data[:hitpoints] <= 3) and (not player.dead?));
+		player.brightness = (player.data[:hitpoints] <= 3) ? 6 : 7;
+	end
+end end
+
 Lasertag::HTTP::Basic.set :game, $game
+Lasertag::HTTP::Basic.set :bind, "0.0.0.0"
 Lasertag::HTTP::Basic.run!
