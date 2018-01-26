56013b6381183475c80afa453ffd1cd9407b3b5f
David Bailey <davidbailey.2889@gmail.com>
2018-01-26 13:28:48 +0100

    Added (very) verboose debug output

--- Ruby/GameInterface/EventHook.rb
+++ Ruby/GameInterface/EventHook.rb
@@ -33,4 +33,33 @@ module Lasertag
 		def onKill(killedPlayer, sourcePlayer)
 		end
 	end
+
+	class VerbooseDebugHook < EventHook
+
+		def onPlayerRegistration(player)
+			puts "#{player.name} was registered!"
+		end
+		def onPlayerUnregistration(player)
+			puts "#{player.name} was unregistered!"
+		end
+
+		def onPlayerConnect(player)
+			puts "#{player.name} connected!"
+		end
+		def onPlayerDisconnect(player)
+			puts "#{player.name} disconnected!"
+		end
+
+		def processHit(hitP, sourceP, arbCode)
+			puts "Confirming hit #{sourceP.name} --> #{hitP.name}! (Code: #{arbCode})";
+			return true;
+		end
+
+		def onHit(hitP, sourceP)
+			puts "Confirmed hit #{sourceP.name} --> #{hitP.name}!"
+		end
+		def onKill(killedP, sourceP)
+			puts "#{sourceP.name} has killed #{killedP.name}!"
+		end
+	end
 end
