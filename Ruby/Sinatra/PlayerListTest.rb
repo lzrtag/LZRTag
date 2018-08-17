31719d63bbc31adea7b5f3cf3689551c2bb23065
Xasin <logger89@web.de>
2018-08-17 13:01:07 +0200

    Fixed PlayerListTest.rb for new code

--- Ruby/Sinatra/PlayerListTest.rb
+++ Ruby/Sinatra/PlayerListTest.rb
@@ -3,61 +3,58 @@
 require 'json'
 require_relative '../GameInterface/LTagGame.rb'
 
-$mqtt = MQTT::SubHandler.new('xasin.hopto.org');
+$mqtt = MQTT::SubHandler.new('localhost');
 $game = Lasertag::Game.new($mqtt);
 
-$game.on_connect do |pName, player|
-	player.ammo = 5;
+class RandomTeam < Lasertag::EventHook
+	def onPlayerConnect(player)
+		player.ammo = 5;
 
-	player.team = rand(1..6);
-	player.brightness = 7;
+		player.team = rand(1..6);
+		player.brightness = 7;
 
-	player.hitConfig = {
-		hitFlashBrightness: 10,
-		hitFlashDuration: 180,
-		hitVibration: 100,
+		player.hitConfig = {
+			hitFlashBrightness: 10,
+			hitFlashDuration: 180,
+			hitVibration: 100,
 
-		deathDuration: 7000,
-	}
+			deathDuration: 7000,
+		}
 
-	player.fireConfig = {
-		shotLocked: false,
-		perShotDelay: 400,
-	}
+		player.fireConfig = {
+			shotLocked: false,
+			perShotDelay: 400,
+		}
 
-	player.data[:hitpoints] = 1.5;
+		player.data[:hitpoints] = 1.5;
+	end
+
+	def onHit(player, shooter)
+		player = $game[data["target"]];
+		if(player.data[:hitpoints] -= 1) <= 0 then
+			player.data[:hitpoints] = 0;
+			player.dead = true;
+		else
+			player.hit
+		end
+		shooter.noise(duration: 0.02, startF: 2000);
+	end
+
+	def onGameTick(dt)
+		$game.each_connected do |pName, player|
+			if(player.data[:hitpoints] < 1.5 and not player.dead?) then
+				player.data[:hitpoints] += 0.15*dt;
+			end
+
+			player.heartbeat = ((player.data[:hitpoints] <= 1) and (not player.dead?));
+			player.brightness = (player.data[:hitpoints] <= 1) ? 6 : 7;
+		end
+	end
 end
+
+$game.add_hook(RandomTeam);
 
 require_relative 'BaseClasses/JSONBase'
-
-$mqtt.subscribe_to "Lasertag/Game/Events" do |topic, data|
-	begin
-		data = JSON.parse(data);
-		if(data["type"] == "hit") then
-			player = $game[data["target"]];
-			if(player.data[:hitpoints] -= 1) <= 0 then
-				player.data[:hitpoints] = 0;
-				player.dead = true;
-			else
-				player.hit
-			end
-			$game[data["shooterID"]].noise(duration: 0.02, startF: 2000);
-		end
-	rescue
-	end
-end
-
-Thread.new do loop do
-	sleep 0.1;
-	$game.each_connected do |pName, player|
-		if(player.data[:hitpoints] < 1.5 and not player.dead?) then
-			player.data[:hitpoints] += 0.015;
-		end
-
-		player.heartbeat = ((player.data[:hitpoints] <= 1) and (not player.dead?));
-		player.brightness = (player.data[:hitpoints] <= 1) ? 6 : 7;
-	end
-end end
 
 Lasertag::HTTP::Basic.set :game, $game
 Lasertag::HTTP::Basic.set :bind, "0.0.0.0"
