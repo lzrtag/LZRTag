301f81daddb386732fbdb62c9a518c72bb8cc94c
David Bailey <davidbailey.2889@gmail.com>
2018-10-24 17:06:01 +0200

    Small test changes

--- Ruby/LasertagBase/lib/lzrtag/test.rb
+++ Ruby/LasertagBase/lib/lzrtag/test.rb
@@ -1,16 +1,33 @@
 
-require_relative 'handler.rb'
 
-class DBG
-	def initialize()
-	end
 
-	def consume_event(evtName, data)
-		puts "Caught event: #{evtName} with data: #{data}";
+module Lasertag
+	class EventHook
+		def initialize()
+		end
+
+		def on_hookin()
+		end
+
+		def processRawHit(*args)
+			return true;
+		end
+
+		def consume_event(evtName, data)
+			puts "Caught event: #{evtName} with data: #{data}";
+		end
 	end
 end
 
-$mqtt = MQTT::SubHandler.new("192.168.251.1");
-$handler = LZRTag::Handler.new($mqtt);
+require_relative 'handler/hitArb_handler.rb'
 
-$handler.add_hook(DBG);
+$mqtt = MQTT::SubHandler.new("192.168.251.1");
+$handler = LZRTag::Handler::HitArb.new($mqtt);
+
+$handler.add_hook(Lasertag::EventHook);
+
+sleep 1;
+
+$handler["White"].fire
+
+sleep 1;
