872774a213c0a1e174664c1df360e3f2d444d067
David Bailey <davidbailey.2889@gmail.com>
2018-10-27 14:41:22 +0200

    Updated test to play with stuff

--- Ruby/LasertagBase/lib/lzrtag/test.rb
+++ Ruby/LasertagBase/lib/lzrtag/test.rb
@@ -21,6 +21,16 @@ end
 DebugHook.on :playerDisconnected do |player|
 	puts "Yay, player #{player.DeviceID} disconnected!"
 end
+DebugHook.on :playerHurt do |player|
+	player.hit();
+end
+DebugHook.on :playerFullyRegenerated do |player|
+	player.dead = false;
+end
+
+DebugHook.on [:playerRegenerated, :playerHurt] do |player|
+	player.heartbeat = (player.life < 30);
+end
 
 $mqtt = MQTT::SubHandler.new("192.168.251.1");
 $handler = LZRTag::Handler::HitArb.new($mqtt);
@@ -29,6 +39,18 @@ $handler.add_hook(DebugHook);
 
 sleep 1;
 
-$handler["White"].fire
+w = $handler["White"];
+w.team = 5;
+w.brightness = 7;
 
-sleep 1;
+until(w.dead)
+	$handler["White"].damage_by(10);
+	sleep 1;
+end
+
+while(w.dead)
+	w.regenerate(20);
+	sleep 1;
+end
+
+sleep 3;
