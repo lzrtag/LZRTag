1493dec516a9e290d8986d969e5731ffe6fb4de3
David Bailey <davidbailey.2889@gmail.com>
2018-10-31 17:02:21 +0100

    Added a few nice standard hooks

--- Ruby/LasertagBase/lib/lzrtag/hooks/standard_hooks.rb
+++ Ruby/LasertagBase/lib/lzrtag/hooks/standard_hooks.rb
@@ -45,7 +45,6 @@ module LZRTag
 
 			def reassignTeam(player)
 				minCount = @handler.teamCount.values.min[1];
-
 				(@teamWhitelist.shuffle()).each do |t|
 					if(@handler.teamCount[t] == minCount)
 						player.team = t;
@@ -55,9 +54,119 @@ module LZRTag
 			end
 
 			on :playerConnected do |player|
-
 				puts "Reassigning player: #{player}"
 				reassignTeam(player);
+			end
+		end
+
+		class Regenerator < Base
+			def initialize(regRate: 1, regDelay: 10, healDead: false, autoReviveThreshold: 30)
+				super();
+
+				@regRate = regRate;
+				@regDelay = regDelay;
+
+				@healDead = healDead;
+				@autoReviveThreshold = autoReviveThreshold;
+			end
+
+			on :gameTick do |dT|
+				@handler.each do |pl|
+					if((Time.now() - pl.lastDamageTime) >= @regDelay)
+						pl.regenerate(dT * @regRate);
+					end
+
+					if(pl.dead and pl.life >= @autoReviveThreshold)
+						pl.dead = false;
+					end
+				end
+			end
+		end
+
+		class Damager < Base
+			def initialize(dmgPerShot: 40, friendlyFire: false, dmgDead: false)
+				super();
+
+				@dmgPerShot = dmgPerShot;
+				@friendlyFire = friendlyFire;
+				@dmgDead = dmgDead;
+			end
+
+			def process_raw_hit(hitPlayer, sourcePlayer)
+				unless(@friendlyFire)
+					return false if hitPlayer.team == sourcePlayer.team
+				end
+				unless(@dmgDead)
+					return false if hitPlayer.dead
+				end
+
+				return true;
+			end
+
+			on :playerHit do |hitPlayer, sourcePlayer|
+				hitPlayer.damage_by(@dmgPerShot, sourcePlayer);
+				hitPlayer.hit();
+			end
+		end
+
+		class Configurator < Base
+			attr_accessor :inGameB, :outGameB
+			attr_reader   :fireConfig, :hitConfig
+
+			def initialize()
+				super();
+
+				@fireConfig = Hash.new();
+				@hitConfig = Hash.new();
+
+				@outGameB = 1;
+				@inGameB  = 7;
+			end
+
+			def fireConfig=(fC)
+				@fireConfig = fC || Hash.new();
+
+				if(@handler and @handler.gameRunning)
+					@handler.each do |pl|
+						pl.fireConfig = @fireConfig;
+					end
+				end
+			end
+			def hitConfig(hC)
+				@hitConfig = hC || Hash.new();
+				if(@handler)
+					@handler.each do |pl|
+						pl.hitConfig = @hitConfig;
+					end
+				end
+			end
+
+			on :gameStarted do
+				@handler.each do |pl|
+					if(@fireConfig)
+						pl.fireConfig = @fireConfig;
+					end
+					pl.brightness = @inGameB;
+					pl.ammo = @fireConfig[:ammoCap] || 5;
+				end
+			end
+			on :gameStopping do
+				@handler.each do |pl|
+					if(@fireConfig)
+						pl.fireConfig = nil;
+					end
+					pl.brightness = @outGameB;
+				end
+			end
+
+			on :playerConnected do |pl|
+				if(@handler.gameRunning)
+					pl.fireConfig = @fireConfig;
+					pl.ammo = @fireConfig[:ammoCap] || 5;
+					pl.brightness = @inGameB;
+				else
+					pl.brightness = @outGameB;
+				end
 			end
 		end
 	end
