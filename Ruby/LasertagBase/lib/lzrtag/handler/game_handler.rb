e852d813f662a5c41e0c91ad3ed414f788095057
David Bailey <davidbailey.2889@gmail.com>
2018-10-31 17:04:14 +0100

    Changed game start/stop handling into a separate thread, always asynch

--- Ruby/LasertagBase/lib/lzrtag/handler/game_handler.rb
+++ Ruby/LasertagBase/lib/lzrtag/handler/game_handler.rb
@@ -3,8 +3,9 @@ require_relative 'count_handler.rb'
 
 module LZRTag
 	module Handler
-		class Game
+		class Game < Count
 			attr_reader :currentGame
+			attr_reader :gameRunning
 
 			def initialize(*data)
 				super(*data)
@@ -14,41 +15,49 @@ module LZRTag
 				@lastGame    = nil;
 				@currentGame = nil;
 
+				@gameRunning = false;
+
+				_start_game_thread();
+			end
+
+			def _start_game_thread()
 				@gameTickThread = Thread.new() do
 					loop do
-						Thread.stop() until(@currentGame);
+						Thread.stop() until(@nextGame.is_a? LZRTag::Game::Base);
 
-						sleep @currentGame.tickTime
-						dT = Time.now() - @lastTick;
-						@lastTick = Time.now();
+						@currentGame = @nextGame;
+						send_event(:gameStarting, @currentGame)
+						@currentGame._on_start_raw();
 
-						send_event(:gameTick, dT);
+						@gameRunning = true;
+						send_event(:gameStarted, @currentGame);
+
+						while(@currentGame == @nextGame)
+							sleep @currentGame.tickTime
+							dT = Time.now() - @lastTick;
+							@lastTick = Time.now();
+
+							send_event(:gameTick, dT);
+						end
+
+						@gameRunning = false;
+						send_event(:gameStopping, @currentGame);
+						@currentGame._on_end_raw();
+						send_event(:gameStopped, @currentGame);
+						@currentGame = @nextGame;
 					end
 				end
-			end
-
-			def _start_game_synch(game)
-				@currentGame = game;
-				@lastGame = game;
-
-				send_event(:gameStarting)
-				@currentGame.on_start();
-
-				send_event(:gameStarted);
-				@gameTickThread.run();
-			end
-
-			def _stop_game_synch()
-				return unless @currentGame
+				@gameTickThread.abort_on_exception = true;
 			end
 
 			def start_game(game = @lastGame)
-				Thread.new() do
-					if(@currentGame)
-						_stop_game_synch();
-					end
-					_start_game_synch(game || @currentGame);
+				unless(game.is_a? LZRTag::Game::Base)
+					raise ArgumentError, "Game class needs to be specified!"
 				end
+				@lastGame = game;
+
+				@nextGame = game;
+				@gameTickThread.run();
 			end
 		end
 	end
