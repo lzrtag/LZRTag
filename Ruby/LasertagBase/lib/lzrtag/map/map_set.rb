965279400981a54d940e9c138cfb774aab5df2d0
David Bailey <davidbailey.2889@gmail.com>
2018-11-18 15:21:55 +0100

    Small addition to map publishing

--- Ruby/LasertagBase/lib/lzrtag/map/map_set.rb
+++ Ruby/LasertagBase/lib/lzrtag/map/map_set.rb
@@ -4,16 +4,25 @@ module LZRTag
 		class Set
 			attr_reader :zones
 
+			attr_accessor :centerpoint
+
 			def initialize(mqtt)
 				@mqtt = mqtt;
 				@zones = Array.new();
 			end
 
 			def to_h()
-				outZones = Array.new();
+				outData = Hash.new();
 
-				@zones.each do ||
+				if(@centerpoint.nil? or @centerpoint.length != 3)
+					raise ArgumentError, "Center point needs to be set!"
+				end
 
+				outData[:centerpoint] = @centerpoint
+
+				outData[:zones] = Array.new();
+				@zones.each do |z|
+					outData[:zones] << z.to_h;
 				end
 
 				return outZones;
@@ -24,7 +33,7 @@ module LZRTag
 			end
 
 			def publish()
-				@mqtt.publish_to "Lasertag/Game/Zones", self.to_json, qos: 1, retain: true;
+				@mqtt.publish_to "Lasertag/Zones", self.to_json, qos: 1, retain: true;
 			end
 			def clear()
 				@mqtt.publish_to "Lasertag/Game/Zones", "", retain: true;
