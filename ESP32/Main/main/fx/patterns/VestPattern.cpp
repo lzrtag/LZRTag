a9c9eb251f06ef69589d4c2f73774b080f40df85
Xasin <davidbailey.2889@gmail.com>
2019-09-06 13:08:02 +0200

    Finished the first batch of time and position-functions

--- ESP32/Main/main/fx/patterns/VestPattern.cpp
+++ ESP32/Main/main/fx/patterns/VestPattern.cpp
@@ -16,6 +16,20 @@ namespace LZR {
 namespace FX {
 
 VestPattern::VestPattern() : BasePattern() {
+
+	time_func = time_func_t::LINEAR;
+	timefunc_period = 600;
+	timefunc_p1_period = 300;
+	timefunc_trap_percent = 0.5 * (1<<16);
+
+	pattern_func = pattern_func_t::SINE;
+
+	sine_center = ((1<<15)-1);
+	sine_amplitude = ((1<<15)-1);
+
+	pattern_period = 5*255;
+	pattern_p1_length = 3*255;
+	pattern_trap_percent = 0.3 * (1<<16);
 }
 
 uint16_t VestPattern::get_normized_trapez(int32_t pos, uint16_t trap_percent) {
@@ -23,15 +37,15 @@ uint16_t VestPattern::get_normized_trapez(int32_t pos, uint16_t trap_percent) {
 
 	if(pos < 0)
 		return 0;
-	if(pos >= (2<<16))
+	if(pos >= (1<<16))
 		return 0;
 
 	if(pos < trap_percent)
-		return ((2<<16 -1) * pos)/trap_percent;
-	else if(pos > ((2<<16 -1) - trap_percent))
-		return ((2<<16 -1) * (2<<16 - pos -1))/trap_percent;
+		return (((1<<16) -1) * pos)/trap_percent;
+	else if(pos > (((1<<16) -1) - trap_percent))
+		return (((1<<16) -1) * ((1<<16) - pos -1))/trap_percent;
 
-	return 2<<16 - 1;
+	return (1<<16) - 1;
 }
 
 uint16_t VestPattern::get_timefunc_shifted(int32_t ticks) {
@@ -45,10 +59,10 @@ uint16_t VestPattern::get_timefunc_shifted(int32_t ticks) {
 
 	case time_func_t::LINEAR:
 		if(tickCnt > timefunc_p1_period)
-			return 2<<16 -1;
+			return (1<<16) -1;
 		if(tickCnt < 0)
 			return 0;
-		return ((2<<16-1)*tickCnt)/timefunc_p1_period;
+		return (((1<<16)-1)*tickCnt)/timefunc_p1_period;
 
 	case time_func_t::TRAPEZ:
 		return get_normized_trapez(((2<<16)*tickCnt)/timefunc_p1_period, timefunc_trap_percent);
@@ -56,15 +70,45 @@ uint16_t VestPattern::get_timefunc_shifted(int32_t ticks) {
 	case time_func_t::HALF_SINE:
 		if(tickCnt > timefunc_p1_period)
 			return 0;
-		return (2<<16-1)*sin((M_PI*tickCnt) / timefunc_p1_period);
+		return ((1<<16)-1)*sin((M_PI*tickCnt) / timefunc_p1_period);
 
 	case time_func_t::EQUAL_SINE:
-		return (2<<15) + (2<<15) * sin((M_PI*tickCnt) / timefunc_p1_period);
+		return (1<<15) -1 + (1<<15) * sin((M_PI*tickCnt) / timefunc_p1_period);
 	}
 }
 
 uint16_t VestPattern::get_patternfunc_at(float pos) {
+	int32_t bitPos = ((1<<8)-1) * pos;
 
+	switch(pattern_func) {
+	default: return 0;
+
+	case pattern_func_t::SINE: {
+		int32_t sVal = sine_center + sine_amplitude * sin(2*M_PI*(bitPos / float(pattern_period) + get_timefunc_shifted(0)/float(1<<16)));
+		if(sVal < 0)
+			return 0;
+		if(sVal > ((1<<16) -1))
+			return ((1<<16) -1);
+		return sVal;
+	}
+
+	case pattern_func_t::TRAPEZ: {
+		bitPos += pattern_p1_length/2 - pattern_period*get_timefunc_shifted(0);
+		bitPos %= pattern_period;
+		bitPos  = (bitPos<<16) / pattern_p1_length;
+
+		return	get_normized_trapez(bitPos, pattern_trap_percent);
+	}
+
+	case pattern_func_t::CHASE_TRAPEZ: {
+		return 0;
+	}
+
+	}
+}
+
+void VestPattern::apply_color_to(Peripheral::Color &tgt, float pos) {
+	tgt.merge_overlay(overlayColor, get_patternfunc_at(pos) >> 8);
 }
 
 } /* namespace FX */
--- ESP32/Main/main/fx/patterns/VestPattern.h
+++ ESP32/Main/main/fx/patterns/VestPattern.h
@@ -13,7 +13,7 @@
 namespace LZR {
 namespace FX {
 
-enum class pattern_type_t {
+enum class pattern_func_t {
 	SINE,
 	TRAPEZ,
 	CHASE_TRAPEZ,
@@ -27,12 +27,6 @@ enum class time_func_t {
 
 class VestPattern: public BasePattern {
 private:
-
-	time_func_t time_func;
-	int32_t timefunc_period;
-	int32_t timefunc_p1_period;
-	int32_t timefunc_trap_percent;
-
 	// This returns a time-normized trapez function, with the top being (1-trap_percent) long
 	// trap_percent = 65535 equals a triangle wave, 65535 ticks long
 	uint16_t get_normized_trapez(int32_t pos, uint16_t trap_percent);
@@ -42,9 +36,24 @@ private:
 	uint16_t get_patternfunc_at(float pos);
 
 public:
+	time_func_t time_func;
+	int32_t timefunc_period;
+	int32_t timefunc_p1_period;
+	int32_t timefunc_trap_percent;
+
+	pattern_func_t pattern_func;
+	int32_t sine_center;
+	int32_t sine_amplitude;
+
+	int32_t 	pattern_period;
+	int32_t 	pattern_p1_length;
+	uint16_t 	pattern_trap_percent;
+
 	Peripheral::Color overlayColor;
 
 	VestPattern();
+
+	void apply_color_to(Peripheral::Color &tgt, float pos);
 };
 
 } /* namespace FX */
