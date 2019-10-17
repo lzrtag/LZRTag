7691ef1e1d06a09ea6d3781d2f98e44b51d749de
David Bailey <davidbailey.2889@gmail.com>
2019-10-17 16:10:56 +0200

    Document the base game functionality

--- Ruby/LasertagBase/lib/lzrtag/game/base_game.rb
+++ Ruby/LasertagBase/lib/lzrtag/game/base_game.rb
@@ -8,10 +8,35 @@ module LZRTag
 		# It implements a DSL that allows users to easily
 		# define their own games, and hooks in with the
 		# game event system
+		# @example
+		#   class OwnGame < LZRTag::Game::Base
+		#      def initialize(handler)
+		#         super(handler);
+		#         @tickTime = 1;
+		#      end
+		#
+		#      hook :dmgHook, LZRTag::Hook::Damager
+		#
+		#      phase :running do |deltaTick|
+		#         puts "I tick!"
+		#      end
+		#
+		#      on :playerRegistered do |newPlayer|
+		#         puts "Hello new player!";
+		#      end
+		#   end
+		#
+		#   handler.start_game(OwnGame)
+		#   # Alternatively, register the game and let it be activated via MQTT
+		#   handler.register_game("My Game", OwnGame);
 		class Base < Hook::Base
+			# Returns a list of all currently instantiated hooks
 			attr_reader :hookList
+			# Returns the current per-tick target time. Can, and should, be set during
+			# constructor to control the granularity of the game
 			attr_reader :tickTime
 
+			# Returns a list of the known phases of this game
 			attr_reader :phases
 
 			# @private
@@ -22,21 +47,21 @@ module LZRTag
 				return @globalPhaseMap;
 			end
 			# @private
-			# See get_phsae_map()
+			# @see get_phase_map()
 			def self.get_phase_prep_map()
 				@globalPhasePrepMap ||= Hash.new();
 				return @globalPhasePrepMap;
 			end
 			# @private
-			# See get_phase_map()
+			# @see get_phase_map()
 			def self.get_phase_end_map()
 				@globalPhaseEndMap ||= Hash.new();
 				return @globalPhaseEndMap;
 			end
 			# @private
-			# See get_phase_map()
+			# @see get_phase_map()
 			def self.get_hooks()
-				@globalHookList ||= Array.new();
+				@globalHookList ||= Hash.new();
 				return @globalHookList;
 			end
 
@@ -57,7 +82,7 @@ module LZRTag
 				super(handler)
 
 				@hookList = Array.new();
-				self.class.get_hooks().each do |hookData|
+				self.class.get_hooks().each do |hookID, hookData|
 					@hookList << hookData[0].new(@handler, **hookData[1])
 				end
 
@@ -73,14 +98,29 @@ module LZRTag
 				@phaseLastTime = 0;
 			end
 
-			def self.hook(hookType, hookOptions = {})
+			# @!group DSL functions
+
+			# DSL function to add a Hook type to this game.
+			# Any hook type added by this function will be instantiated when the game
+			# itself is instantiated, and will be linked in with the internal game
+			# signals.
+			# @param hookID [Symbol] The ID of the hook, used for later referencing
+			# @param hookType [Hook::Base] The class of the hook to instantiate
+			# @param hookOptions [Hash] A hash of options to pass to the constructor of the hook
+			def self.hook(hookID, hookType, hookOptions = {})
+				raise ArgumentError, "Hook ID needs to be a symbol!" unless hookID.is_a? Symbol
 				unless hookType.is_a? Class and hookType < LZRTag::Hook::Base
 					raise ArgumentError, "Hook needs to be a LZR::Hook!"
 				end
 				raise ArgumentError, "Hook options need to be a hash" unless hookOptions.is_a? Hash
-				get_hooks() << [hookType, hookOptions];
+				get_hooks()[hookID] << [hookType, hookOptions];
 			end
 
+			# DSL function to provide a phase tick code to this game
+			# The block provided to this function will be executed every game tick,
+			# with the delta-time since last tick as parameter.
+			# @param phaseName [Symbol] The name of the phase during which to execute this code
+			# @yield [deltaTime] Calls this block every game-tick during the specified phase
 			def self.phase(phaseName, &block)
 				raise ArgumentError, "Block needs to be given!" unless block_given?
 
@@ -93,6 +133,11 @@ module LZRTag
 				end
 			end
 
+			# DSL function to provide a callback immediately after switching to a new phase.
+			# The provided block will be called only once, right after a phase switch happened.
+			# As such, it can be used to prepare game timer settings, player classes, etc.
+			# @param phaseName [Symbol] The name of the phase before which to execute
+			# @yield [oldPhase] Calls the block right after a switch, with the old phase as parameter
 			def self.phase_prep(phaseName, &block)
 				raise ArgumentError, "Block needs to be given!" unless block_given?
 
@@ -106,6 +151,11 @@ module LZRTag
 				end
 			end
 
+			# DSL function to provide a callback immediately before switching to a new phase.
+			# The provided block is guaranteed to execute before any phase and phase_prep blocks,
+			# giving the user an option to reset and clean up after themselves.
+			# @param phaseName [Symbol] Name of the phase after which to call this block
+			# @yield [newPhase] Calls the block right before the switch, with the upcoming phase as parameter
 			def self.phase_end(phaseName, &block)
 				raise ArgumentError, "Block needs to be given!" unless block_given?
 
@@ -119,6 +169,9 @@ module LZRTag
 				end
 			end
 
+			# @!endgroup
+
+			# @private
 			def consume_event(evt, data)
 				super(evt, data);
 
@@ -140,6 +193,7 @@ module LZRTag
 				end
 			end
 
+			# @private
 			def handle_phase_change
 				@phaseTime = 0;
 
@@ -149,6 +203,7 @@ module LZRTag
 				end
 			end
 
+			# @private
 			def handle_game_tick(dT)
 				phase = @handler.gamePhase
 				return unless @phaseMap[phase];
