c8cb4cf29aef14371701dc0498c8fdbeaaafe20b
David Bailey <davidbailey.2889@gmail.com>
2019-10-18 09:19:52 +0200

    Document the core handler classes

--- Ruby/LasertagBase/lib/lzrtag/handler/game_handler.rb
+++ Ruby/LasertagBase/lib/lzrtag/handler/game_handler.rb
@@ -3,10 +3,43 @@ require_relative 'count_handler.rb'
 
 module LZRTag
 	module Handler
+=begin
+	Game handler class, managing game registration and game ticks.
+	This class manages the lifecycle of any lasertag game started in it.
+	This includes sending a gameTick, managing phase and game state transitions,
+	sending out but also receiving information on the game state via MQTT, etc.
+
+@example
+	handler = LZRTag::Handler::Game.new(mqtt);
+
+	handler.register_game("My Game", SomeGame);
+	handler.register_game("Other game!", SomeOtherGame);
+
+	handler.start_game(SomeGame); # This will fetch the registered name and publish to MQTT
+											# Alternatively, sending to Lasertag/Game/Controls/SetGame with
+											# the string name will switch to the game.
+
+	# Game and phase switching is possible at any time, and will be handled asynchronously
+	sleep 5;
+	handler.set_phase(:somePhase);
+=end
 		class Game < Count
+			# Returns the instance of the currently active game, or nil if none is present
 			attr_reader :currentGame
+
+			# currently active game phase
+			# When set it will start a phase change, sending :gamePhaseEnds and
+			# :gamePhaseStarts events
+			# @see set_phase
+			# @return Symbol
 			attr_reader :gamePhase
 
+			# List of in-game players.
+			# This list is mainly to keep track of which players the game is acting
+			# upon, but updating it will also send a list of player ID Strings to
+			# MQTT (Lasertag/Game/ParticipatingPlayers), and will send :playerEnteredGame
+			# and :playerLeftGame events
+			# @return Array<LZRTag::Player::Base>
 			attr_reader :gamePlayers
 
 			def initialize(*data, **argHash)
@@ -53,6 +86,7 @@ module LZRTag
 				@mqtt.publish_to "Lasertag/Game/KnownGames", [].to_json, retain: true;
 				@mqtt.publish_to "Lasertag/Game/CurrentGame", "", retain: true
 			end
+			private :clean_game_topics;
 
 			def _start_game_thread()
 				@gameTickThread = Thread.new() do
@@ -79,7 +113,15 @@ module LZRTag
 				end
 				@gameTickThread.abort_on_exception = true;
 			end
+			private :_start_game_thread;
 
+			# Register a game by a tag.
+			# This function will register a given LZRTag::Game::Base class under a given
+			# string tag. This tag can then be used to, via MQTT, start the game, and is
+			# also used to give players a cleartext game name.
+			# A list of games is published to Lasertag/Game/KnownGames
+			# @param gameTag [String] Cleartext name of the game
+			# @param game [LZRTag::Game::Base] The game class to register
 			def register_game(gameTag, game)
 				raise ArgumentError, "Game Tag must be a string!" unless gameTag.is_a? String
 				raise ArgumentError, "Game must be a LZRTag::Game class" unless game <= LZRTag::Game::Base
@@ -89,6 +131,7 @@ module LZRTag
 				@mqtt.publish_to "Lasertag/Game/KnownGames", @knownGames.keys.to_json, retain: true;
 			end
 
+			# @private
 			def consume_event(event, data)
 				super(event, data)
 
@@ -96,8 +139,20 @@ module LZRTag
 				@currentGame.consume_event(event, data);
 			end
 
+			# Starts a given new game (or the last one).
+			# This function will take either a String (as registered with register_game),
+			# or a LZRTag::Game::Base class, instantiate it, and start it.
+			# If no fitting game was found, the game is instead stopped.
+			# @param game [String,LZRTag::Game::Base] The game, or game name, to start
 			def start_game(game = @lastGame)
 				@lastGame = game;
+
+				if(game.is_a? String and gClass = @knownGames[game])
+					game = gClass;
+				elsif(game.is_a? String)
+					stop_game();
+					return;
+				end
 
 				if(gKey = @knownGames.key(game))
 					@mqtt.publish_to "Lasertag/Game/CurrentGame", gKey, retain: true
@@ -117,11 +172,14 @@ module LZRTag
 					get_allowed_phases.to_json(), retain: true
 			end
 
+			# Stops the currently running game.
 			def stop_game()
 				@nextGame = nil;
 				@mqtt.publish_to "Lasertag/Game/CurrentGame", "", retain: true
 			end
 
+			# Returns an Array<Symbol> of the currently allowed phases of this game.
+			# This list can also be retrieved via MQTT, under Lasertag/Game/CurrentGame
 			def get_allowed_phases()
 				allowedPhases = [:idle]
 				if(@currentGame)
@@ -131,6 +189,13 @@ module LZRTag
 				return allowedPhases;
 			end
 
+			# Tries to change the current phase.
+			# This function will set the current phase to nextPhase, if it is an allowed
+			# one. However, if nextPhase does not belong to the list of allowed phases,
+			# and error is raised. The :gamePhaseEnds and :gamePhaseStarts events are
+			# triggered properly. This function can be called from any context, not just
+			# inside the game code itself.
+			# @see get_allowed_phases
 			def set_phase(nextPhase)
 				allowedPhases = get_allowed_phases();
 
@@ -145,27 +210,45 @@ module LZRTag
 				@mqtt.publish_to "Lasertag/Game/Phase/Current", @gamePhase.to_s, retain: true
 
 			end
+			# Alias for set_phase
+			# @see set_phase
 			def gamePhase=(nextPhase)
 				set_phase(nextPhase)
 			end
 
+			# Update the list of in-game players.
+			# @param [Array<LZRTag::Player::Base] Array of active players
 			def gamePlayers=(newPlayers)
 				raise ArgumentError, "Game player list shall be an array!" unless newPlayers.is_a? Array
-				@gamePlayers = newPlayers;
+				@gamePlayers = newPlayers.dup;
 
+				@playerNames = Array.new();
 				plNameArray = Array.new();
 				@gamePlayers.each do |pl|
 					plNameArray << pl.deviceID();
 				end
 
+				newPlayers = @gamePlayers - @oldGamePlayers;
+				newPlayers.each do |pl|
+					send_event :playerEnteredGame, pl;
+				end
+				oldPlayers = @oldGamePlayers - @gamePlayers;
+				oldPlayers.each do |pl|
+					send_event :playerLeftGame, pl;
+				end
+
+				@oldGamePlayers = @gamePlayers.dup;
 				@mqtt.publish_to "Lasertag/Game/ParticipatingPlayers", plNameArray.to_json(), retain: true
 			end
+
+			# Yield for each currently in-game player
 			def each_participating()
 				@gamePlayers.each do |pl|
 					yield(pl)
 				end
 			end
 
+			# Check if a player is currently in game
 			def in_game?(player)
 				return @gamePlayers.include? player
 			end
